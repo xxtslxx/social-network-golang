1e616e2eaa38e2a3ce81675c72d38396b33f1d97
xxtslxx <tassiolimar01@gmail.com>
2024-02-05 12:34:29 -0300

    terceiro commit do projeto

--- api/sql/sql.sql
+++ api/sql/sql.sql
@@ -2,6 +2,7 @@ CREATE DATABASE IF NOT EXISTS devbook;
 
 USE devbook;
 
+DROP TABLE IF EXISTS publicacoes;
 DROP TABLE IF EXISTS usuarios;
 DROP TABLE IF EXISTS seguidores;
 
@@ -28,4 +29,16 @@ CREATE TABLE seguidores(
     primary key(usuario_id, seguidor_id)
 ) ENGINE=INNODB;
 
+CREATE TABLE publicacoes(
+    id int auto_increment primary key,
+    titulo varchar(50) not null,
+    conteudo varchar(300) not null,
 
+    autor_id int not null,
+    FOREIGN KEY (autor_id)
+    REFERENCES usuarios(id)
+    ON DELETE CASCADE,
+
+    curtidas int default 0,
+    criadaEm timestamp default current_timestamp
+) ENGINE=INNODB;
---
+++ api/src/router/rotas/publicacoes.go
@@ -0,0 +1,39 @@
+package rotas
+
+import (
+	"api/src/controllers"
+	"net/http"
+)
+
+var rotasPublicacoes = []Rota{
+	{
+		URI:    "/publicacoes",
+		Metodo: http.MethodPost,
+		Funcao: controllers.CriarPublicacoes,
+		RequerAutenticacao: true,
+	},
+	{
+		URI:    "/publicacoes",
+		Metodo: http.MethodGet,
+		Funcao: controllers.BuscarPublicacoes,
+		RequerAutenticacao: true,
+	},
+	{
+		URI:    "/publicacoes",
+		Metodo: http.MethodGet,
+		Funcao: controllers.BuscarPublicacao,
+		RequerAutenticacao: true,
+	},
+	{
+		URI:    "/publicacoes",
+		Metodo: http.MethodPut,
+		Funcao: controllers.AtualizarPublicacao,
+		RequerAutenticacao: true,
+	},
+	{
+		URI:    "/publicacoes",
+		Metodo: http.MethodDelete,
+		Funcao: controllers.DeletarPublicacao,
+		RequerAutenticacao: true,
+	},
+}
